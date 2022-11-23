fd9e0317bac57060498f0f1ace9f6068d3b9e9d6
richard.zhang <zjh991600@163.com>
2022-11-22 23:26:35 -0500

    Initial commit

---
+++ src/main/java/com/zhangjgux/easygarage/EasyGarageApplication.java
@@ -0,0 +1,13 @@
+package com.zhangjgux.easygarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyGarageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyGarageApplication.class, args);
+    }
+
+}
